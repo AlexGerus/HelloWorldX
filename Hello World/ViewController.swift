1faea8bce23fe589dcb80d491c252c785e32a57a
Alex Gerus <alexander.gerus@grse.de>
2024-10-09 13:49:52 +0300

    Init

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Alexander Gerus on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
